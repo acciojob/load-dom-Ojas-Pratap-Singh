eb67eb566ef81254a8d200f15d4b4d2bbe24c93e
Ojas Pratap Singh <128834924+Ojas-Pratap-Singh@users.noreply.github.com>
2023-11-29 19:04:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',()=>{
+    let body = document.querySelector("body");
+   let ptag = document.createElement("p");   
+   ptag.innerText="DOM load success";
+   body.append(ptag);
+});
